375fe138ae2e8d3fe2baba6fc5c79fb2fea607bb
Rocky Chen <jinweizsu@gmail.com>
2023-05-25 18:11:07 +0800

    v1

--- vue-project/demo1/src/App1.vue
+++ vue-project/demo1/src/App1.vue
@@ -3,19 +3,27 @@
     <input type="text" v-model="inputValue" @blur="clearInput">  
     <p>输入框失去焦点时的值：{{ inputValue }}</p>  
   </div>  
+
+  <div v-if="isShow">is show {{ isShow }}</div>
+  <button @click="clickme">click me</button>
 </template>  
   
 <script>  
 export default {  
   data() {  
     return {  
-      inputValue: ''  
+      inputValue: '',  
+        isShow:true
     }  
   },  
   methods: {  
     clearInput() {  
       this.inputValue = ''  
-    }  
-  }  
-}  
+    },
+      clickme(){
+      this.isShow = !this.isShow
+      }
+  }
+}
+
 </script>
---
+++ vue-project/demo1/src/App2.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <p v-if="showMessage">这是一条显示的消息</p>  
+  <p v-else>这是一条隐藏的消息</p>  
+</div>
+</template>
+
+<script>
+const App1 = {
+name:'App1',
+    data(){
+        return {
+            showMessage:true
+        }
+    }
+
+}
+export default App1
+</script>
+
